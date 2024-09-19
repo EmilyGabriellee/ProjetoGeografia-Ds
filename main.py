9f75553ad114bb3d894ca43816ea9fb0325dd592
papagaio <163757749+papagaiogamer@users.noreply.github.com>
2024-09-19 08:35:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,131 @@
-from questionario import Resposta,db
+from pymongo import MongoClient
+from datetime import date
 import os
+import time
+import random 
 
-db.connect()
+id_random = random.randint(0,9999999)
 
-db.create_tables([Resposta])
-os.system('color a')
+client = MongoClient('localhost',27017)#conectar banco de dados
 
-nome = str(input('Qual o seu nome?\n'))
-assaltos = int(input('Houve algum assalto recentemente?Se sim,quantos?\n'))
-parente = str(input('Você possui algum conhecido que foi assaltado recentemente?(y/n)\n'))
-viatura = int(input('Há um bom numero de viaturas em ronda?\n'))
-base = int(input('Existe alguma base policial 24H por perto?Se sim,quantas?\n'))
-nota = 0
+db = client.segurancaplus
 
-
-if assaltos > 5 or parente == 'y':
-    nota+=3
-if viatura < 10 and base < 2:
-    nota+=6
-elif viatura < 10 and base > 2:
-    nota+=3
-elif viatura > 10 and base < 2:
-    nota+=3    
-else:
-    print('pipoca')
+seguranca = db.registro
+while True:
+    os.system('color 6')
+    os.system('cls')
+    
+    print('sistema de boletim de ocorrencia\n\n [1]registrar ocorrencia\n [2]conferir registro\n [3]deletar registro')
+    entrar = int(input(' '))
+    #registrar ocorrencia
+    os.system('cls')
+    os.system('color f')
+    if entrar == 1:
+        
+        name = str(input('Digite o seu nome: '))
+        sobrenome = str(input('Digite sobrenome: '))
+        cpf = str(input('Insira cpf: '))
+        descricao = str(input('O que aconteceu:\n'))
+        hj = str(date.today())
+        os.system('cls')
+        print('processando')
+        time.sleep(1)
+        os.system('cls')
+        print('processando.')
+        time.sleep(1)
+        os.system('cls')
+        print('processando..')
+        time.sleep(1)
+        os.system('cls')
+        print('processando...')
+        time.sleep(1)
+        os.system('cls')
+        print('Ocorrencia registrada')
+        
+        seguranca.insert_one({
+            '_id': id_random,
+            'nome': name,
+            'sobrenome': sobrenome,
+            'CPF': cpf,
+            'data_registro':hj ,
+            'descricao': descricao  
+        })
+        input(' ')
+        os.system('cls')
+    #conferir registros
+    elif entrar == 2:
+        os.system('color 1')
+        print('buscar por:\n [1]nome\n [2]data de registro\n [3]data da ocorrencia\n [4]mostrar todos os registros')
+        busca = int(input(' '))
+        if busca == 1:
+            nome_busca =  str(input('insira o nome\n'))
+            os.system('cls')
+            print('buscando')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando.')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando..')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando...')
+            time.sleep(1)
+            os.system('cls')
+            print('resultados:\n')
+            
+            for i in seguranca.find({"nome":nome_busca}):
+                print(i)
+            input(' ')
+                
+        elif busca == 2:
+            print('insira a data de registro(siga esse modelo:ano-mês-dia')
+            registro = str(input(' '))
+            os.system('cls')
+            print('buscando')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando.')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando..')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando...')
+            time.sleep(1)
+            os.system('cls')
+            print('resultados:\n')
+            for i in seguranca.find({'data_registro':registro}):
+                print(i)
+            input(' ')
+        else:
+            os.system('cls')
+            print('buscando')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando.')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando..')
+            time.sleep(1)
+            os.system('cls')
+            print('buscando...')
+            time.sleep(1)
+            os.system('cls')
+            print('resultados:\n')
+            for i in seguranca.find({}):
+                print(i)
+            input(' ')
+        os.system('cls')
+    elif entrar ==3:
+        deletar = int(input('insira ID do registro'))
+        for i in seguranca.find({'_id': deletar}):
+            print(i)
+        confirmar = int(input('deseja deletar registro?\n [1]sim\n [2]não'))
+        if confirmar == 1:
+            seguranca.delete_one({"_id": deletar})
+        else:
+            print('voltando')
+    else:
+        break
     
